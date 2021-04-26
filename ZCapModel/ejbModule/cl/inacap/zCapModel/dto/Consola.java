fbb3b6342f41183a3d164b37c9a8c8f5b4712682
David <davidandres551@gmail.com>
2021-04-26 11:20:16 -0400

    generar la base del proyecto ear

---
+++ ZCapModel/ejbModule/cl/inacap/zCapModel/dto/Consola.java
@@ -0,0 +1,12 @@
+package cl.inacap.zCapModel.dto;
+
+import java.time.LocalDate;
+
+public class Consola {
+	private String nombre;
+	private String descripcion;
+	private boolean aptoNinios;
+	private int precio;
+	private LocalDate fechaLanzamiento;
+	private Consola consola;
+}
